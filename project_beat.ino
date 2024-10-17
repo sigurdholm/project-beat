f150e1bf05f937340e253d883b6384d1ff85de8a
Sigurd Holm <sigurd@tiimo.dk>
2024-10-17 22:24:40 +0200

    Working MVP

--- project_beat.ino
+++ project_beat.ino
@@ -4,31 +4,41 @@
 
 #include "secrets.h"
 
+// WIFI
 const char *ssid = WIFI_SSID;
 const char *password = WIFI_PASSWORD;
 
+// Brooker connection
 const char *mqtt_broker = BROOKER_URL;
 const char *mqtt_username = AUTH_USERNAME;
 const char *mqtt_password = AUTH_PASSWORD;
 const char *mqtt_topic = TOPIC;
 const int mqtt_port = PORT;
-
 const char *ca_cert = CA_CERTIFICATE;
 
+// Clients
 BearSSL::WiFiClientSecure esp_client;
 PubSubClient mqtt_client(esp_client);
 
 // NTP Server settings
 const char *ntp_server = "pool.ntp.org";
-const long gmt_offset_sec = 7200;            // GMT offset in seconds (adjust for your time zone)
+const long gmt_offset_sec = 7200; // GMT offset in seconds (adjust for your time zone)
 const int daylight_offset_sec = 0;  
+
+const char single_char_id = SINGLE_CHAR_ID;
+
+// Hardware
+const int button_pin = D6;
+
+uint8_t led_state = LOW;
+
+uint8_t button_state;
+uint8_t previous_button_state = LOW;
 
 void connectToWifi();
 void connectToMQTTBroker();
 void syncTime();
 void mqttCallback(char *topic, byte *payload, unsigned int length);
-
-
 
 void setup() {
   Serial.begin(115200);
@@ -38,17 +48,22 @@ void setup() {
   mqtt_client.setCallback(mqttCallback);
   connectToMQTTBroker();
 
-  pinMode(LED_BUILTIN, OUTPUT);  // Initialize the LED_BUILTIN pin as an output
+  // Hardware setup
+  pinMode(button_pin, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH); // HIGH means off
 }
 
 void connectToWifi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.setPhyMode(WIFI_PHY_MODE_11G);
   WiFi.begin(ssid, password);
-  Serial.print("Connecting to WiFi");
+  Serial.println("Connecting to WiFi");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(".");
+    Serial.printf("Wifi status: %d \n", WiFi.status());
   }
-  Serial.println("\nConnected to the WiFi network");
+  Serial.println("Connected to the WiFi network");
 }
 
 void syncTime() {
@@ -78,7 +93,6 @@ void connectToMQTTBroker() {
     if (mqtt_client.connect(client_id.c_str(), mqtt_username, mqtt_password)) {
       Serial.println("Connected to MQTT broker");
       mqtt_client.subscribe(mqtt_topic);
-      mqtt_client.publish(mqtt_topic, "IS ANYONE THERE??");
     } else {
       char err_buf[128];
       esp_client.getLastSSLError(err_buf, sizeof(err_buf));
@@ -100,11 +114,35 @@ void mqttCallback(char *topic, byte *payload, unsigned int length) {
   }
   Serial.println();
   Serial.println("-----------------------");
+
+  if ((char) payload[0] == single_char_id) {
+    Serial.println("Own message xD");
+    return;
+  }
+
+  if ((char) payload[1] == '1') {
+    digitalWrite(LED_BUILTIN, LOW); // LOW means on
+  } else {
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
 }
 
 void loop() {
+  if (WiFi.status() != WL_CONNECTED) {
+    connectToWifi();
+  }
+
   if (!mqtt_client.connected()) {
       connectToMQTTBroker();
   }
   mqtt_client.loop();
+
+  button_state = digitalRead(button_pin);
+
+  if (button_state != previous_button_state) {
+    char state_to_send = button_state == HIGH ? '1' : '0';
+    char payload[3] = {single_char_id, state_to_send, '\0'};
+    mqtt_client.publish(mqtt_topic, payload);
+    previous_button_state = button_state;
+  }
 }
